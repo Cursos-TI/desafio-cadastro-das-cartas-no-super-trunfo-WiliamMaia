1cf366324a72f1bfa49ff36fb6266f05451b869f
WiliamMaia <wiliam.maia.silva@gmail.com>
2025-02-21 11:02:39 +0000

    # Trabalhei mais no projeto, adicionando variaveis para diferenciar # a carta 1 da carta 2 para futuras melhorias e funcionalidades # Espero ter compreendido o desafio corretamente e espero um feedback # sobre qualquer erro, o que faltou e se estou no caminho certo do # projeto, obrigado por me avaliar !

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -6,42 +6,105 @@
 // Siga os comentários para implementar cada parte do desafio.
 //Teste larissa
 
-int main() {
+int main(){
 
-	char identificacao[3];
-	char cidade[20];
-	int pontosturisticos, area;
-	float populacao;
-	double PIB;
+    // Aqui declaramos as variaveis que serão: 
+    // Carta 1 como exemplo
+    // char Estado: A
+    // char Código: 01
+    // char Nome da Cidade: ???
+    // int População: ????????
+    // float Área: ????.?? km²
+    // float PIB: ???.?? bilhões de reais
+    // int Número de Pontos Turísticos: ??
 
-    printf("Vamos cadastrar uma carta!\n\n");
+	char estado1, estado2;
+    char codigo1[4], codigo2[4];
+    char nomedacidade1[20], nomedacidade2[20];
+    int populacao1, populacao2;
+    float area1, area2;
+    float PIB1, PIB2;
+    int numerodepontosturisticos1, numerodepontosturisticos2;
 
-	printf("Digite o código da carta, por exemplo 'A01': \n");
-	scanf("%s", &identificacao);
+    //  Aqui solicitaremos o cadastro da carta
+    // Todo 'printf' será a solicitação da informação
+    // Todo 'scanf' será a entrada de dados
 
-	printf("Digite o nome da Cidade: \n");
-	scanf("%s", &cidade);
+    printf("Vamos cadastrar uma carta !\n");
+	printf("Começaremos com a carta 1\n\n");
+    printf("____________________\n\n");
 
-	printf("Digite a população: \n");
-	scanf("%f", &populacao);
+    printf("Qual será a letra que representará o grupo do estado? Por exemplo, A do grupo A\n");
+        scanf(" %c", &estado1);
 
-	printf("Digite a área: \n");
-	scanf("%d", &area);
+    printf("Qual o número da carta? Adicione a letra também para atribuí-la ao grupo. Exemplo: A01\n");
+        scanf(" %s", &codigo1);
 
-	printf("Digite o PIB: \n");
-	scanf("%f", &PIB);
+    printf("Qual o nome da cidade? Por favor sem espaços e acentuação\n");
+        scanf(" %s", &nomedacidade1);
 
-	printf("Quantos ponros turísticos?: \n");
-	scanf("%d", &pontosturisticos);
+    printf("Informe a população apenas com números: \n");
+        scanf(" %d", &populacao1);
 
-	printf("Carta criada!\n\n");
+    printf("Qual a àrea? \n");
+        scanf(" %f", &area1);
 
-	printf("Carta: %s\n", identificacao);
-	printf("Cidade: %s\n", cidade);
-	printf("População: %.2f\n", populacao);
-	printf("Área: %d\n", area);
-	printf("PIB: %f\n", PIB);
-	printf("Pontos Turísticos: %d\n", pontosturisticos);
+    printf("Qual é o PIB? \n");
+        scanf(" %f", &PIB1);
 
-    return 0;
+    printf("Quanrtos pontos turísticos? \n");
+        scanf(" %d", &numerodepontosturisticos1);
+
+// Aqui exibirei os dados coletados
+
+	printf("____________________\n\n");
+    printf("A carta 1 ficou assim: \n\n");
+    printf("Esrado grupo: %c\n", estado1);
+    printf("Código: %s\n", codigo1);
+    printf("Nome da Cidade: %s\n", nomedacidade1);
+    printf("População: %d\n", populacao1);
+    printf("Área: %.2f km²\n", area1);
+    printf("PIB: %.2f bilhões de reais\n", PIB1);
+    printf("Número de pontos turísticos: %d\n\n", numerodepontosturisticos1);
+
+// Qui coletaremos as informações para a carta 2
+
+	printf("Agora a carta 2\n\n");
+    printf("____________________\n\n");
+
+	printf("Qual será a letra que representará o grupo do estado? Por exemplo, A do grupo A\n");
+	scanf(" %c", &estado2);
+
+	printf("Qual o número da carta? Adicione a letra também para atribuí-la ao grupo. Exemplo: A01\n");
+	scanf(" %s", &codigo2);
+
+	printf("Qual o nome da cidade? Por favor sem espaços e acentuação\n");
+	scanf(" %s", &nomedacidade2);
+
+	printf("Informe a população apenas com números: \n");
+	scanf(" %d", &populacao2);
+
+	printf("Qual a àrea? \n");
+	scanf(" %f", &area2);
+
+	printf("Qual é o PIB? \n");
+	scanf(" %f", &PIB2);
+
+	printf("Quanrtos pontos turísticos? \n");
+	scanf(" %d", &numerodepontosturisticos2);
+
+	printf("____________________\n\n");
+    printf("A carta 2 ficou assim: \n\n");
+    printf("Esrado grupo: %c\n", estado2);
+    printf("Código: %s\n", codigo2);
+    printf("Nome da Cidade: %s\n", nomedacidade2);
+    printf("População: %d\n", populacao2);
+    printf("Área: %.2f km²\n", area2);
+    printf("PIB: %.2f bilhões de reais\n", PIB2);
+    printf("Número de pontos turísticos: %d\n\n", numerodepontosturisticos2);
+
+	printf("Pronto !\n");
+
+return 0;
+
 }
