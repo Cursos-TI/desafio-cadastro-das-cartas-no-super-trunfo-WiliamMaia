34830e5e278412a7faf7647ee225e40f1aa2019f
WiliamMaia <wiliam.maia.silva@gmail.com>
2025-02-22 00:02:14 +0000

    # Procurei adaptar para o nível mestre do tema 1, adicionando o "Super Poder", rive de aplicar algumas alterações para garantir que os valores sejam mantidos exatos utilizando do unsigned int, long e long long double, além de ter mudado alguns valores de float para double.

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -19,16 +19,18 @@ int main(){
     // int Número de Pontos Turísticos: ??
     // float Densidade Populacional: ???
     // float PIB per Capita: R$ ????
+    // float Super Poder: ??????
 
 	char estado1, estado2;
     char codigo1[4], codigo2[4];
     char nomedacidade1[20], nomedacidade2[20];
-    int populacao1, populacao2;
-    float area1, area2;
-    float PIB1, PIB2;
+    unsigned int populacao1, populacao2;  // Alterei para unsigned int para garantir que o valor seja mantido.
+    double area1, area2;  // Precisei alterarrpara double para o valor ser exato
+    long double PIB1, PIB2;  // Precisei mudar para long double para o valor ser exato
     int numerodepontosturisticos1, numerodepontosturisticos2;
     float densidadepopulacional1, densidadepopulacional2;
     float PIBpercapta1, PIBpercapta2;
+    long double superpoder1, superpoder2;  // Precisei mudar para long double para o balor ser exato
 
     //  Aqui solicitaremos o cadastro da carta
     // Todo 'printf' será a solicitação da informação
@@ -48,34 +50,37 @@ int main(){
         scanf(" %s", &nomedacidade1);
 
     printf("Informe a população apenas com números: \n");
-        scanf(" %d", &populacao1);
+        scanf(" %u", &populacao1);  // Alterei de %i para %u agora que é undigned int
 
     printf("Qual a àrea? \n");
-        scanf(" %f", &area1);
+        scanf(" %lf", &area1);  // alterei para %lf por ser long double
 
     printf("Qual é o PIB? \n");
-        scanf(" %f", &PIB1);
+        scanf(" %Lf", &PIB1);  // Alterei para %Lf por ser long long double
 
     printf("Quanrtos pontos turísticos? \n");
         scanf(" %d", &numerodepontosturisticos1);
 
-    densidadepopulacional1 = (float) populacao1 / area1; // Aqui calculamos a Densidade Populacional
+    densidadepopulacional1 = (double) populacao1 / area1; // Aqui calculamos a Densidade Populacional
 
-    PIBpercapta1 = (float) PIB1 / populacao1; // Aqui calculamos o PIB per Capita
+    PIBpercapta1 = (long double) PIB1 / populacao1; // Aqui calculamos o PIB per Capita
+
+    superpoder1 = (long double) populacao1 + area1 + PIB1 + numerodepontosturisticos1 + PIBpercapta1 - densidadepopulacional1;
 
 // Aqui exibirei os dados coletados
 
-	printf("____________________\n\n");
+    printf("____________________\n\n");
     printf("A carta 1 ficou assim: \n\n");
     printf("Esrado grupo: %c\n", estado1);
     printf("Código: %s\n", codigo1);
     printf("Nome da Cidade: %s\n", nomedacidade1);
-    printf("População: %d\n", populacao1);
-    printf("Área: %.2f km²\n", area1);
-    printf("PIB: %.2f de reais\n", PIB1);
+    printf("População: %u\n", populacao1);
+    printf("Área: %.3f km²\n", area1);
+    printf("PIB: R$ %.2Lf\n", PIB1);
     printf("Número de Pontos Turísticos: %d\n", numerodepontosturisticos1);
     printf("Densidade Populacional: %.2f\n", densidadepopulacional1);  // Adicionada a Densidade Populacional do Nível Aventureiro Tema 1
-    printf("PIB per Capita: R$ %.2f\n\n", PIBpercapta1); // Adicionado o PIB per Capta do Nível Aventureiro Tema 1
+    printf("PIB per Capita: R$ %.2f\n", PIBpercapta1); // Adicionado o PIB per Capta do Nível Aventureiro Tema 1
+    printf("Super Poder: %.2Lf\n\n", superpoder1); // Adicionado "Super Poder" que é a soma dos atributos numéricos com a Densidade Populacional invertida
 
 // Agora coletaremos as informações para a carta 2
 
@@ -92,32 +97,35 @@ int main(){
 	scanf(" %s", &nomedacidade2);
 
 	printf("Informe a população apenas com números: \n");
-	scanf(" %d", &populacao2);
+	scanf(" %u", &populacao2);
 
 	printf("Qual a àrea? \n");
-	scanf(" %f", &area2);
+	scanf(" %lf", &area2);
 
 	printf("Qual é o PIB? \n");
-	scanf(" %f", &PIB2);
+	scanf(" %Lf", &PIB2);
 
 	printf("Quanrtos pontos turísticos? \n");
 	scanf(" %d", &numerodepontosturisticos2);
 
-    densidadepopulacional2 = (float) populacao2 / area2;
+    densidadepopulacional2 = (double) populacao2 / area2;
 
-    PIBpercapta2 = (float) PIB2 / populacao2;
+    PIBpercapta2 = (long double) PIB2 / populacao2;
+
+    superpoder2 = (long double) populacao2 + area2 + PIB2 + numerodepontosturisticos2 + PIBpercapta2 - densidadepopulacional2;
 
 	printf("____________________\n\n");
     printf("A carta 2 ficou assim: \n\n");
     printf("Esrado grupo: %c\n", estado2);
     printf("Código: %s\n", codigo2);
     printf("Nome da Cidade: %s\n", nomedacidade2);
-    printf("População: %d\n", populacao2);
+    printf("População: %u\n", populacao2);
     printf("Área: %.2f km²\n", area2);
-    printf("PIB: %.2f de reais\n", PIB2);
+    printf("PIB: R$ %.2Lf\n", PIB2);
     printf("Número de Pontos Turísticos: %d\n", numerodepontosturisticos2);
     printf("Densidade Populacional: %.2f\n", densidadepopulacional2);
-    printf("PIB per Capita: R$ %.2f\n\n", PIBpercapta2);
+    printf("PIB per Capita: R$ %.2f\n", PIBpercapta2);
+    printf("Super Poder: %.2Lf\n\n", superpoder2);
 
 	printf("Pronto !\n");
 
